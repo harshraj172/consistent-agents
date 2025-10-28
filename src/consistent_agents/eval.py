dca1e21a9a2f2680df246388f8708c639437cd37
Suvrorup Mukherjee <rabi.rishi@gmail.com>
2025-10-28 18:57:10 -0400

    resolving 'env' issue in evaluate fn: eval.py

--- src/consistent_agents/eval.py
+++ src/consistent_agents/eval.py
@@ -140,7 +140,7 @@ def evaluate(
     metric_scores: Dict[str, Any] = {metric.name(): None for metric in metrics}
 
     for item in tqdm(benchmark.iter(), desc="Evaluating", unit="ex", total=len(benchmark)):
-        base_output = agent_fn(item["question"])
+        base_output = agent_fn(item["question"], benchmark.env)
         item["base_output"] = base_output
 
     # for item in tqdm(items, desc="Evaluating", unit="ex"):
