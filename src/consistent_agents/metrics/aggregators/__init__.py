be083dc1cb660c514f3e5a7e788f4c6ead4db604
Niranjan Krishna <niranjankrishna.acad@gmail.com>
2025-11-19 22:57:48 +0530

    added metrics

---
+++ src/consistent_agents/metrics/aggregators/__init__.py
@@ -0,0 +1,5 @@
+from consistent_agents.metrics.aggregators.pairwise import pairwise
+from consistent_agents.metrics.aggregators.entropy import entropy
+
+__all__ = ["pairwise", "entropy"]
+
---
+++ src/consistent_agents/metrics/agreement_functions/__init__.py
@@ -0,0 +1,6 @@
+from consistent_agents.metrics.agreement_functions.llm_judge import llm_judge
+from consistent_agents.metrics.agreement_functions.bertscore import bertscore
+from consistent_agents.metrics.agreement_functions.rouge import rouge
+
+__all__ = ["llm_judge", "bertscore", "rouge"]
+
