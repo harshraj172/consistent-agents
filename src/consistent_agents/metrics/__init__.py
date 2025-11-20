9d0037b477a6d466297c87610b9b68c4987e0f6e
Niranjan Krishna <niranjankrishna.acad@gmail.com>
2025-11-20 23:55:50 +0530

    added tests

--- src/consistent_agents/metrics/__init__.py
+++ src/consistent_agents/metrics/__init__.py
@@ -3,12 +3,10 @@ from consistent_agents.metrics.agreement_functions import consistency, contradic
 from consistent_agents.metrics.aggregators import pairwise, entropy
 from consistent_agents.metrics.accuracy import score as accuracy_score
 from consistent_agents.metrics.consistency import score as consistency_score
-from consistent_agents.metrics.entailment import score as entailment_score
-from consistent_agents.metrics.contradiction import score as contradiction_score
 
 __all__ = [
     "consistency", "contradiction", "entailment", "bertscore", "rouge",
     "pairwise", "entropy",
-    "accuracy_score", "consistency_score", "entailment_score", "contradiction_score",
+    "accuracy_score", "consistency_score",
 ]
 
